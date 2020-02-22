04a065def318655c47dc8267309e8e2ae3407a47
Nicolas Gonzalez <nicolas.g96@gmail.com>
2020-02-22 10:34:22 -0700

    run on port indicated by command line argument

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ let app = express();
 let http = require('http').createServer(app);
 require('./serverSideJs/setup.js')(require('socket.io')(http));
 
+const port = parseInt(process.argv[2]);
+if (isNaN(port)){
+	console.log("Invalid port number passed in. Program will exit.");
+	process.exit();
+}
+
 // allow files in public directory to be served as static files
 app.use(express.static('public'));
 
@@ -10,7 +16,6 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(parseInt(process.argv[2]), function(){
-	console.log('argv[2] == the port to run on == ' + process.argv[2])
-	console.log('listening on *:' + process.argv[2]);
+http.listen(port, function(){
+	console.log('listening on *:' + port);
 });
